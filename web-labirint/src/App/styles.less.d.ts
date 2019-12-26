840349932d94e2484111218f2fac20db5cc37d0b
Adisey <iadisey@gmail.com>
2019-12-26 23:54:14 +0200

    was added Less Modules

---
+++ web-labirint/src/App/styles.less.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly "App": string;
+  readonly "AppHeader": string;
+  readonly "AppLogo": string;
+  readonly "AppLink": string;
+  readonly "AppLogoSpin": string;
+};
+export = styles;
+
