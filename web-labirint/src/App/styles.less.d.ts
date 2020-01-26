e3ba57a8b9d0bb12d9b10ae67166e3ba524cf828
Adisey <iadisey@gmail.com>
2020-01-26 21:36:09 +0200

    begin

--- web-labirint/src/App/styles.less.d.ts
+++
@@ -1,9 +0,0 @@
-declare const styles: {
-  readonly "App": string;
-  readonly "AppHeader": string;
-  readonly "AppLogo": string;
-  readonly "AppLink": string;
-  readonly "AppLogoSpin": string;
-};
-export = styles;
-
